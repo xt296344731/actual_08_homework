0216ae83df808069f3cbdb820c0ee86d8b802642
perry385817606 <385817606@qq.com>
2016-03-05 23:20:37 +0800

    day01 homework by fangjun
    day01 homework by fangjun

---
+++ 01/fangjun/zuoye.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python27
+#
+NumList = [1,2,-3,2,65535,12,3,-1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+
+max1 = None
+max2 = None
+for i in NumList:
+    if i > max1:
+        tmp = max1
+        max1 = i
+        i = tmp
+    if max1 > max2:
+        max1,max2 = max2,max1
+
+print max1
+print max2
+         
+
